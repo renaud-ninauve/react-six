7ad4bd992a26ccc0d0b80c15721a89176c1b2ca1
renaud-ninauve <renaud.ninauve.92@gmail.com>
2020-03-22 17:05:27 +0100

    [wip] Add cards distribution

--- src/index.js
+++ src/index.js
@@ -13,29 +13,100 @@ class Card extends React.Component {
   }
 }
 
-class Deck extends React.Component {
+class PlayerDeck extends React.Component {
     render() {
-      let cards = this.props.cards.map(card => (<Card value={card.value} cows={card.cows}/>));
+      let cards = this.props.cards.map(
+        (card, i) => (<Card value={card.value} cows={card.cows} key={i}/>));
       return (
-        <div className="deck">
+        <div className="player-deck">
           {cards}
         </div>
       );
     }
 }
 
+class CardStack extends React.Component {
+  render() {
+    let cards = this.props.cards.map(
+      (card, i) => (<Card value={card.value} cows={card.cows} key={i}/>));
+
+    return (
+      <div className="card-stack">
+        {cards}
+      </div>
+    );
+  }
+}
+
+class CardMat extends React.Component {
+  render() {
+    let cardStacks = this.props.cardMat.map(
+      (cards, i) => (<CardStack cards={cards} key={i}/>));
+    return (
+      <div className="card-mat">
+        {cardStacks}
+      </div>
+    );
+  }
+}
+
 class Game extends React.Component {
   render() {
-    let cards = [{value: 3, cows: 6}, {value: 3, cows: 6}, {value: 3, cows: 6}];
     return (
-        <Deck cards={cards} />
+        <div>
+          <CardMat cardMat={this.props.distribution.cardMat}/>
+          <PlayerDeck cards={this.props.distribution.player}/>
+        </div>
     );
   }
 }
 
 // ========================================
 
+let valueCows = Array(35).fill(1);
+valueCows[5] = 2;
+valueCows[10] = 3;
+valueCows[11] = 5;
+valueCows[15] = 2;
+valueCows[20] = 3;
+valueCows[22] = 5;
+valueCows[25] = 2;
+valueCows[30] = 3;
+valueCows[33] = 5;
+
+function shuffle(arr) {
+  for(let i = arr.length - 1; i > 0; i--){
+    const j = Math.floor(Math.random() * i)
+    const temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+  }
+}
+
+function createCardFromValue(value) {
+  return {value: value, cows: valueCows[value]};
+}
+
+function distribute() {
+  let cards = Array(34).fill().map((_, i) => i+1).map(createCardFromValue);
+  shuffle(cards);
+
+  let player = cards.slice(0, 10);
+  let cpu1 = cards.slice(10, 20);
+  let cpu2 = cards.slice(20, 30);
+  let cardMat = cards.slice(30).map((card, i) => [card]);
+
+  return {
+    player: player,
+    cpu1: cpu1,
+    cpu2: cpu2,
+    cardMat: cardMat
+  };
+}
+
+console.log(distribute());
+
 ReactDOM.render(
-  <Game />,
+  <Game distribution={distribute()}/>,
   document.getElementById('root')
 );
